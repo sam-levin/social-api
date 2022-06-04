f7ad7a6b519c9e68f8946ab5dd7deff4b1622f85
sam-levin <sam.ky.levin@gmail.com>
2022-06-04 12:09:52 -0700

    adding thoughts route

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,6 @@ const thoughtRoutes = require('./api/thought-routes');
 // add prefix of `/pizzas` to routes created in `pizza-routes.js`
 //router.use('/thoughts', thoughtRoutes)
 router.use('/users', userRoutes);
+router.use('/thoughts', thoughtRoutes)
 
 module.exports = router;
