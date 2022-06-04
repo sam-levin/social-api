88c6c9d189d39f0e85b8aeee698a75bacad1066e
sam-levin <sam.ky.levin@gmail.com>
2022-06-04 16:42:57 -0700

    initial reaction texts

--- models/Thought.js
+++ models/Thought.js
@@ -32,6 +32,33 @@ const ThoughtSchema = new Schema({
     id: false
 })
 
+const ReactionSchema = new Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId();
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        // code here to give it a max of 280 characters
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => dateFormat(createdAtVal)
+    }
+}
+,
+{
+    toJSON: {
+        getters: true
+    },
+    id: false
+})
 
 // ThoughtSchema.virtual('reactionCount').get(function(){
 //     return this.reaction.length;
