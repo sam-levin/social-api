d1712d77cf88be03969e6850bad79cd59aced86a
sam-levin <sam.ky.levin@gmail.com>
2022-06-04 12:21:41 -0700

    updating user controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -17,13 +17,41 @@ const userController = {
         .catch(err => res.status(400).json(err))
     },
     getUserById( { params }, res){
-
+        User.findOne({ _id: params.id })
+            .select('-__')
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({message: 'No user found with this id'});
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => {
+                console.log(err)
+                res.status(400).json(err);
+            })
     },
     updateUser( {params, body}, res){
-
+        User.findOneAndUpdate({ _id:params.id}, body, {new: true, runValidators: true})
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'Cannot update user. No user found with this id'})
+                    return;
+                }
+                res.json(dbUserData)
+            })
+            .catch(err => res.status(400).json(err));
     },
     deleteUser({ params }, res){
-
+        User.findOneAndDelete({ _id: params.id})
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'Cannot delete user. No user found with this id'})
+                    return
+                }
+                res.json(dbUserData)
+            })
+            .catch(err => res.status(400).json(err))
     }
 }
 
